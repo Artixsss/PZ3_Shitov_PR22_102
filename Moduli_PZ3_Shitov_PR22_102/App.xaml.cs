97c50f7817f3bc5da24455e71326413a5f1cacc8
artixs <Musicartoum@mail.ru>
2024-10-14 00:02:10 +0700

    Initial commit

---
+++ Moduli_PZ3_Shitov_PR22_102/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Moduli_PZ3_Shitov_PR22_102
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Moduli_PZ3_Shitov_PR22_102/Client.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Moduli_PZ3_Shitov_PR22_102
+{
+    /// <summary>
+    /// Логика взаимодействия для Client.xaml
+    /// </summary>
+    public partial class Client : Page
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+    }
+}
